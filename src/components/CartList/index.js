d2ac3ea15b2de44fb85809159a7956b034d8c61f
izzudin26 <izzudin.ar26@gmail.com>
2021-11-27 01:18:48 +0700

    complete cart

--- src/components/CartList/index.js
+++ src/components/CartList/index.js
@@ -1,147 +1,190 @@
-import React, { useState, useContext, createContext } from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native'
+import React, {useState, useContext, createContext} from 'react';
+import {StyleSheet, Text, View, Image} from 'react-native';
 import RNPickerSelect from 'react-native-picker-select';
-import { Chevron } from 'react-native-shapes';
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import {Chevron} from 'react-native-shapes';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
 
 let JumlahHarga = createContext();
 
-const JumlahHargaProvider = ({ children }) => {
-    const [listTotal, setListTotal] = useState({});
+const JumlahHargaProvider = ({children}) => {
+  const [listTotal, setListTotal] = useState({});
 
-    return (
-        <JumlahHarga.Provider value={[listTotal, setListTotal]}>
-            {children}
-        </JumlahHarga.Provider>
-    );
+  return (
+    <JumlahHarga.Provider value={[listTotal, setListTotal]}>
+      {children}
+    </JumlahHarga.Provider>
+  );
 };
 
-const CartList = ({ harga, satuan, listTotal, setListTotal, setEndTotal }) => {
-    const [click, setClick] = useState(satuan);
-    const [price, setPrice] = useState(harga);
-    const [total, setTotal] = useState(harga);
+const CartList = ({harga, satuan, listTotal, setListTotal, setEndTotal}) => {
+  const [click, setClick] = useState(satuan);
+  const [price, setPrice] = useState(harga);
+  const [total, setTotal] = useState(harga);
 
-    const placeholder = {
-        label: JSON.stringify(parseInt(satuan)),
-        value: 0,
-        color: '#000',
-    };
+  const placeholder = {
+    label: JSON.stringify(parseInt(satuan)),
+    value: 0,
+    color: '#000',
+  };
 
-    return (
-        <View style={{
-            flexDirection: 'row',
-            alignItems: 'flex-start',
-            justifyContent: 'space-between',
-            marginVertical: 15,
-            width: wp('90%'),
+  return (
+    <View
+      style={{
+        flexDirection: 'row',
+        alignItems: 'flex-start',
+        justifyContent: 'space-between',
+        marginVertical: 15,
+        width: wp('90%'),
+      }}>
+      <View style={styles.ContainerSmallImage}>
+        <Image
+          source={require('../../assets/images/MainImage.png')}
+          style={styles.ImageSmall}
+        />
+      </View>
+      <View
+        style={{
+          flexDirection: 'column',
+          justifyContent: 'space-evenly',
+          height: 70,
         }}>
-            <View style={styles.ContainerSmallImage}>
-                <Image
-                    source={require('../../assets/images/MainImage.png')}
-                    style={styles.ImageSmall}
-                />
-            </View>
-            <View style={{ flexDirection: 'column', justifyContent: 'space-evenly', height: 70 }}>
-                <Text style={{ height: 35, width: wp('37.5%'), fontSize: 25, fontWeight: 'bold', color: 'black' }}>Nama Product</Text>
-                <Text style={{ height: 20, width: wp('37.5%'), fontSize: 15, color: 'black' }}>Ini adalah keterangan product</Text>
-            </View>
-            <View style={{ flexDirection: 'column', alignItems: 'flex-end', justifyContent: 'space-evenly', height: 70, marginTop: 5 }}>
-                <Text style={{ height: 20, fontSize: 15, color: 'black' }}>Rp. {total}</Text>
-                <RNPickerSelect
-                    style={{
-                        ...pickerSelectStyles,
-                        placeholder: {
-                            color: '#333',
-                            fontSize: 14,
-                            fontWeight: 'normal',
-                        },
-                        iconContainer: {
-                            top: 8,
-                            left: 28,
-                        },
-                    }}
-                    Icon={() => {
-                        return <Chevron size={1} color="#333" />;
-                    }}
-                    useNativeAndroidPickerStyle={false}
-                    onValueChange={(value) => {
-                        setTotal(value + price);
-                        setEndTotal(value + price);
+        <Text
+          style={{
+            height: 35,
+            width: wp('37.5%'),
+            fontSize: 25,
+            fontWeight: 'bold',
+            color: 'black',
+          }}>
+          Nama Product
+        </Text>
+        <Text
+          style={{
+            height: 20,
+            width: wp('37.5%'),
+            fontSize: 15,
+            color: 'black',
+          }}>
+          Ini adalah keterangan product
+        </Text>
+      </View>
+      <View
+        style={{
+          flexDirection: 'column',
+          alignItems: 'flex-end',
+          justifyContent: 'space-evenly',
+          height: 70,
+          marginTop: 5,
+        }}>
+        <Text style={{height: 20, fontSize: 15, color: 'black'}}>
+          Rp. {total}
+        </Text>
+        <RNPickerSelect
+          style={{
+            ...pickerSelectStyles,
+            placeholder: {
+              color: '#333',
+              fontSize: 14,
+              fontWeight: 'normal',
+            },
+            iconContainer: {
+              top: 8,
+              left: 28,
+            },
+          }}
+          Icon={() => {
+            return <Chevron size={1} color="#333" />;
+          }}
+          useNativeAndroidPickerStyle={false}
+          onValueChange={value => {
+            setTotal(value + price);
+            setEndTotal(value + price);
 
-                        listTotal = total;
-                        setListTotal(listTotal);
+            listTotal = total;
+            setListTotal(listTotal);
 
-                        // let total2 = 0;
-                        // for (const index in listTotal) {
-                        //     total2 += listTotal[index];
-                        // }
-                        // setEndTotal(total2);
-                    }}
-                    placeholder={placeholder}
-                    items={[
-                        { label: '2', value: (price) },
-                        { label: '3', value: (price * 2) },
-                        { label: '4', value: (price * 3) },
-                    ]}
-                />
-            </View>
-        </View>
-    )
-}
+            // let total2 = 0;
+            // for (const index in listTotal) {
+            //     total2 += listTotal[index];
+            // }
+            // setEndTotal(total2);
+          }}
+          placeholder={placeholder}
+          items={[
+            {label: '2', value: price},
+            {label: '3', value: price * 2},
+            {label: '4', value: price * 3},
+          ]}
+        />
+      </View>
+    </View>
+  );
+};
 
 const TotalView = ({JumlahTotal}) => {
-    return (
-        <View style={{ flexDirection: 'row', alignItems: 'center', width: wp('75%'), justifyContent: 'space-around' }}>
-            <Text style={{ color: 'black', fontWeight: 'bold', fontSize: 40 }}>Total</Text>
-            <Text style={{ color: 'black', fontSize: 40 }}>{JumlahTotal}</Text>
-        </View>
-    )
-}
+  return (
+    <View
+      style={{
+        flexDirection: 'row',
+        alignItems: 'center',
+        width: wp('75%'),
+        justifyContent: 'space-around',
+      }}>
+      <Text style={{color: 'black', fontWeight: 'bold', fontSize: 40}}>
+        Total
+      </Text>
+      <Text style={{color: 'black', fontSize: 40}}>{JumlahTotal}</Text>
+    </View>
+  );
+};
 
-export { CartList, JumlahHarga, JumlahHargaProvider, TotalView };
+export {CartList, JumlahHarga, JumlahHargaProvider, TotalView};
 
 const styles = StyleSheet.create({
-    ContainerSmallImage: {
-        width: wp('25%'),
-        height: 100,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 20,
-        backgroundColor: 'white',
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
+  ContainerSmallImage: {
+    width: wp('25%'),
+    height: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+    backgroundColor: 'white',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    ImageSmall: {
-        width: wp('17.5%'),
-        height: 70
-    }
-})
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  ImageSmall: {
+    width: wp('17.5%'),
+    height: 70,
+  },
+});
 
 const pickerSelectStyles = StyleSheet.create({
-    inputIOS: {
-        fontSize: 16,
-        paddingVertical: 12,
-        paddingHorizontal: 10,
-        borderWidth: 1,
-        borderColor: 'gray',
-        borderRadius: 4,
-        color: 'black',
-        paddingRight: 30, // to ensure the text is never behind the icon
-    },
-    inputAndroid: {
-        fontSize: 16,
-        width: wp('12.5%'),
-        height: 25,
-        color: '#333',
-        backgroundColor: '#cacaca',
-        paddingHorizontal: 13,
-        paddingVertical: 2,
-        borderRadius: 50,
-    },
+  inputIOS: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 4,
+    color: 'black',
+    paddingRight: 30, // to ensure the text is never behind the icon
+  },
+  inputAndroid: {
+    fontSize: 16,
+    width: wp('12.5%'),
+    height: 25,
+    color: '#333',
+    backgroundColor: '#cacaca',
+    paddingHorizontal: 13,
+    paddingVertical: 2,
+    borderRadius: 50,
+  },
 });
--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,45 +1,231 @@
-import React, { useContext, useState } from 'react'
-import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from 'react-native'
-import { CartList, JumlahHarga, TotalView, JumlahHargaProvider } from '../../components/CartList';
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import React, {useContext, useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome5';
 
-const Cart = ({ navigation, route }) => {
-    const [listTotal, setListTotal] = useState({});
-    // const satuan = JSON.stringify(route.params.qty);
-    const [endTotal, setEndTotal] = useState();
+const h = Dimensions.get('window').height;
+const w = Dimensions.get('window').width;
+const Cart = ({navigation, route}) => {
+  const [carts, setCart] = useState([
+    {
+      productName: 'Oranda',
+      price: 10000,
+      qty: 3,
+      description: 'Pancawarna Rostail 15 cm',
+    },
+    {
+      productName: 'Pil Biru',
+      price: 10000,
+      qty: 3,
+      description: 'Pancawarna Rostail 15 cm',
+    },
+    {
+      productName: 'Oranda',
+      price: 10000,
+      qty: 3,
+      description: 'Pancawarna Rostail 15 cm',
+    },
+  ]);
 
+  const _getTotal = () => {
+    let total = 0;
+    carts.map(c => {
+      total += c.price * c.qty;
+    });
+    return total;
+  };
+
+  const Bottom = () => {
     return (
+      <View style={css.bottomLayout}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            width: w * 0.9,
+            alignItems: 'center',
+          }}>
+          <Text style={{color: 'black', fontSize: 30, fontWeight: 'bold'}}>
+            Total
+          </Text>
+          <Text style={{color: 'black', fontSize: 20, fontWeight: 'bold'}}>
+            Rp. {_getTotal()}
+          </Text>
+        </View>
+        <TouchableOpacity style={css.checkoutBtn} onPress={() => {}}>
+          <Text style={{color: '#F5C63F'}}>Check Out</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
+  const _incrementItem = index => {
+    let currentItem = carts;
+    currentItem[index].qty = currentItem[index].qty + 1;
+    setCart([...currentItem]);
+  };
+
+  const _decrementItem = index => {
+    let currentItem = carts;
+    if (currentItem[index].qty > 0) {
+      currentItem[index].qty = currentItem[index].qty - 1;
+      setCart([...currentItem]);
+    }
+  };
+
+  const CartComponent = () => {
+    return carts.map((cart, i) => (
+      <View style={css.cartCard} key={i}>
+        <View style={css.containerImage}>
+          <TouchableOpacity style={css.closeBtn}>
+            <FontAwesome
+              name="times"
+              solid
+              size={15}
+              color="#F60202"></FontAwesome>
+          </TouchableOpacity>
+          <Image source={require('../../assets/images/obat.png')}></Image>
+        </View>
+        <View
+          style={{
+            flexDirection: 'column',
+            width: w * 0.4,
+            marginTop: 15,
+          }}>
+          <Text style={{color: 'black', fontWeight: 'bold', fontSize: 20}}>
+            {cart.productName}
+          </Text>
+          <Text style={{color: 'black'}}>{cart.description}</Text>
+        </View>
+        <View
+          style={{
+            flexDirection: 'column',
+            marginLeft: 5,
+            alignItems: 'flex-end',
+            marginTop: 15,
+          }}>
+          <Text style={{color: 'black', fontWeight: 'bold'}}>
+            Rp. {cart.price}
+          </Text>
+          <View style={css.contaienrValue}>
+            <TouchableOpacity>
+              <FontAwesome
+                name="minus"
+                size={15}
+                color="black"
+                onPress={() => _decrementItem(i)}></FontAwesome>
+            </TouchableOpacity>
+            <Text style={{color: 'black', marginHorizontal: 10}}>
+              {cart.qty}
+            </Text>
+            <TouchableOpacity>
+              <FontAwesome
+                name="plus"
+                size={15}
+                color="black"
+                onPress={() => _incrementItem(i)}></FontAwesome>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    ));
+  };
+
+  return (
+    <View style={css.container}>
+      <View style={css.containerCart}>
         <ScrollView>
-            <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-                <View style={{ alignItems: 'center', justifyContent: 'space-between', marginHorizontal: 20, borderBottomWidth: 1 }}>
-                    <CartList harga={600000} satuan={1} listTotal={listTotal} setListTotal={setListTotal} setEndTotal={setEndTotal}/>
-                    <CartList harga={50000} satuan={1} listTotal={listTotal} setListTotal={setListTotal} setEndTotal={setEndTotal}/>
-                    {/* <CartList idProduk={3} harga={600000} satuan={satuan} listTotal={listTotal} setListTotal={setListTotal} setEndTotal={setEndTotal} />
-                    <CartList idProduk={4} harga={50000} satuan={satuan} listTotal={listTotal} setListTotal={setListTotal} setEndTotal={setEndTotal} /> */}
-                </View>
-                {/* <TotalView JumlahTotal={setEndTotal}></TotalView> */}
-                <TotalView JumlahTotal={600000}></TotalView>
-                <TouchableOpacity
-                    style={{
-                        width: wp('37.5%'),
-                        height: 40,
-                        backgroundColor: '#043C88',
-                        marginVertical: 20,
-                        borderRadius: 10,
-                        alignItems: 'center',
-                        justifyContent: 'center'
-                    }}
-                    onPress={() => navigation.navigate('Shipping')}>
-                    <Text style={{ color: '#F0C341', fontWeight: 'bold' }}> Check Out </Text>
-                </TouchableOpacity>
-            </View>
+          <CartComponent />
         </ScrollView>
-    )
-}
+      </View>
+      <Bottom />
+    </View>
+  );
+};
 
-export default Cart
+export default Cart;
 
-const styles = StyleSheet.create({})
-
-
-
+const css = StyleSheet.create({
+  container: {
+    width: w,
+    height: h * 0.9,
+    padding: 10,
+  },
+  bottomLayout: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerCart: {
+    height: h * 0.75,
+    padding: 20,
+  },
+  cartCard: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    borderRadius: 15,
+    justifyContent: 'space-between',
+    padding: 10,
+  },
+  containerImage: {
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    padding: 10,
+    margin: 5,
+    borderRadius: 15,
+    backgroundColor: '#fff',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 4.65,
+    elevation: 5,
+  },
+  closeBtn: {
+    padding: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 100,
+    marginTop: -15,
+    marginLeft: -15,
+    height: 20,
+    width: 20,
+    backgroundColor: '#fff',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 4.65,
+    elevation: 6,
+  },
+  checkoutBtn: {
+    borderRadius: 15,
+    marginVertical: 20,
+    backgroundColor: '#043C88',
+    paddingHorizontal: 50,
+    paddingVertical: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  contaienrValue: {
+    flexDirection: 'row',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    padding: 5,
+    marginTop: 3,
+  },
+});
