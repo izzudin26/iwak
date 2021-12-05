85bb69ba617746889918a8c2df4a9d4868e5f31e
izzudin26 <izzudin.ar26@gmail.com>
2021-12-05 08:19:24 +0700

    add auction detail

--- src/router/index.js
+++ src/router/index.js
@@ -36,6 +36,7 @@ import {
   ProductFeedback,
   IncomingOrder,
   ListBidAuction,
+  DetailProductAuction,
 } from '../pages';
 import BottomNavigator from '../components/BottomNavigator';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
@@ -1327,6 +1328,50 @@ const Router = () => {
       <Stack.Screen
         name="DetailProductStore"
         component={DetailProductStore}
+        options={({navigation, route}) => ({
+          headerStyle: {
+            backgroundColor: '#043C88',
+            height: 70,
+          },
+          headerTitle: () => (
+            <View>
+              <Text style={styles.teksHeadLoginSignup}> </Text>
+            </View>
+          ),
+          headerLeft: () => {
+            return (
+              <View
+                style={{
+                  backgroundColor: 'white',
+                  justifyContent: 'center',
+                  width: 80,
+                  height: 35,
+                  marginLeft: -30,
+                  borderRadius: 30,
+                  overflow: 'hidden',
+                }}>
+                <FontAwesome5
+                  name="arrow-left"
+                  solid
+                  size={23}
+                  color="#F0C341"
+                  style={{
+                    alignSelf: 'flex-end',
+                    paddingHorizontal: 20,
+                  }}
+                  onPress={() => navigation.pop()}
+                />
+              </View>
+            );
+          },
+          headerBackVisible: false,
+          headerTitleAlign: 'center',
+        })}
+      />
+
+      <Stack.Screen
+        name="DetailProductAuction"
+        component={DetailProductAuction}
         options={({navigation, route}) => ({
           headerStyle: {
             backgroundColor: '#043C88',
